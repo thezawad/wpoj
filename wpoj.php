466ed4f58a6f488adaee0b799f5c31c4a76eaf18
johnnychq <johnnychq@gmail.com>
2011-05-27 05:45:37 +0000

    add problem type's meta box

--- wpoj.php
+++ wpoj.php
@@ -11,18 +11,27 @@ Domain Path: /lang
 
 Copyright (C) 2010 johnnychen(johnnychq@gmail.com)
 GPL V2
-*/
+*/	
 class OJ{
 	function init(){
+		OJ::_init();
 		add_action('init',array(__CLASS__,'_register_post_type'));
+		if ( is_admin() ) {
+			add_action( 'admin_menu', array(__CLASS__,'_regiter_meta_box') );
+		}
+	}
+	function _init(){
+		define('OJ_LIBRARY',dirname(__FILE__).'/library');
+		define('OJ_FUNCTIONS',OJ_LIBRARY.'/functions');
 	}
 	function _register_post_type(){
+		/* register post types */
 		register_post_type("contest",array(
 			'public' => true,
 			'labels' => array(
 				'name' => 'Contests',
 				'singular_name' => 'Contest',
-				'search_item' =>'Search contests',
+				'search_items' =>'Search Contests',
 				'not_found' => 'No contests found'
 			),
 			'has_archive' => 'contests'
@@ -33,7 +42,11 @@ class OJ{
 			'labels' => array(
 				'name' => 'Problems',
 				'singular_name' => 'Problem',
-				'search_items' => 'Search problems',
+				'new_item' => 'New Problem',
+				'edit_item' => 'Edit Problem',
+				'view_item'	=> 'View Problem',
+				'add_new_item' => 'Add New Problem',
+				'search_items' => 'Search Problems',
 				'not_found' => 'No problems found'
 			),
 			'has_archive' =>'problems'
@@ -47,6 +60,7 @@ class OJ{
 				'not_found' => 'No solutions found'
 			)
 		));
+		/* register post types connections*/
 		p2p_register_connection_type(array(
 			'from' => 'contest',
 			'to' => 'problem',
@@ -54,13 +68,28 @@ class OJ{
 			'prevent_duplicates'=> false,
 			'title' => array('from'=>'Include Prolems','to'=>'Included Contest')
 		));
-		p2p_register_connection_type(array(
-			'from' => 'problem',
-			'to' => 'solution',
-			'reciprocal' => false,
-			'prevent_duplicates'=>false,
-			'title' => array('from' => 'Submitted solutions','to'=>'To be submitted')
+		p2p_register_connection_type('solution','problem');
+		/* register post types taxonomies */
+		register_taxonomy("problem_cat", "problem",array(
+			'public' => true,
+			'labels' => array(
+				'name'=>'Problem Categories',
+				'singular_name'=>'Problem Category',
+			),
+			'hierarchical'=> true
 		));
+		register_taxonomy("problem_tag", "problem",array(
+			'public' => true,
+			'labels' => array(
+				'name'=>'Problem Tags',
+				'singular_name'=>'Problem Tag',
+			),
+			'hierarchical'=> false
+		));
+	}
+	function _regiter_meta_box(){
+		require_once (OJ_LIBRARY.'/class/meta_box.php');
+		OJ_meta_box::init();
 	}
 }
 add_action('plugins_loaded', array('OJ','init'));
