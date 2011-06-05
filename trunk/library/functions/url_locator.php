a6e511f9efb69cacaab71b6f8629105db67f7fa6
johnnychq@gmail.com <johnnychq@gmail.com@c4461a03-5b1b-01de-8ed5-c093b130e12b>
2011-06-05 09:01:07 +0000

    增加submitpage页面

--- trunk/library/functions/url_locator.php
+++ trunk/library/functions/url_locator.php
@@ -3,7 +3,29 @@
  * wp-loaded hook callback
  */
 function oj_maybe_redirect_url(){
-	global $top_page;
+	global $top_page,$page_urls,$oj_bread_trail;
+	$page_urls=array(
+		'problem'=>array(
+			'url'=>'<a href="/?oj=problems">Problems</a>',
+			'url_raw' => '/?oj=problems',
+			'label' => 'Problems'
+		),
+		'statusl' =>array(
+			'url' => '<a href="/?oj=statusl">Status</a>',
+			'url_raw' => '/?oj=statusl',
+			'label' =>'Status',
+		),
+		'statusd' =>array(
+			'url' => '<a href="/?oj=statusd">Status Detail</a>',
+			'url_raw' => '/?oj=statusd',
+			'label' =>'Status Detail',
+		),
+		'submitpage' =>array(
+			'url' => '<a href="/?oj=submitpage">Submit Page</a>',
+			'url_raw' => '/?oj=submitpage',
+			'label' =>'Submit Page',
+		)
+	);
 	$page=$_GET['oj'];
 	if(empty($page)){
 		$top_page ='home';
@@ -12,20 +34,37 @@ function oj_maybe_redirect_url(){
 	switch ($page){
 		case 'statusl':
 			$top_page='status';
+			$oj_bread_trail['trail_end']='Status';
 			locate_template('oj-status-list.php',true);
 			exit(0);
 			break;
 		case 'statusd':
+			if($_GET['title']){
+				$top_page='problem';
+				$oj_bread_trail[]=$page_urls['problem']['url'];
+				$oj_bread_trail[]='<a href="/?problem='.$_GET['title'].'">'.$_GET['title'].'</a>';
+				$oj_bread_trail['trail_end']='Problem Status';
+			}else{
+				$top_page='status';
+				$oj_bread_trail[]=$page_urls['status']['url'];
+				$oj_bread_trail['trail_end']='Status Detail';
+			}
 			locate_template('oj-status-detail.php',true);
 			exit(0);
 			break;
 		case 'problems':
 			$top_page='problem';
+			$oj_bread_trail['trail_end']='Problem';
 			locate_template('oj-problems.php',true);
 			exit(0);
 			break;
-		case 'solution-submit':
-			echo "hfs";
+		case 'submitpage':
+			$top_page='problem';
+			$oj_bread_trail[]=$page_urls['problem']['url'];
+			$oj_bread_trail[]='<a href="/?problem='.$_GET['title'].'">'.$_GET['title'].'</a>';
+			$oj_bread_trail['trail_end']='Submit Solution';
+			locate_template('oj-submitpage.php',true);
+			exit(0);
 			break;
 	}
 }
