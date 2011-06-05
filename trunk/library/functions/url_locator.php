619953b3804b96c361bde3f48aa93b8a38500e99
johnnychq@gmail.com <johnnychq@gmail.com@c4461a03-5b1b-01de-8ed5-c093b130e12b>
2011-06-05 14:18:19 +0000

    addsolution.php 提交问题初稿

--- trunk/library/functions/url_locator.php
+++ trunk/library/functions/url_locator.php
@@ -4,6 +4,7 @@
  */
 function oj_maybe_redirect_url(){
 	global $top_page,$page_urls,$oj_bread_trail;
+	$oj_bread_trail=array();
 	$page_urls=array(
 		'problem'=>array(
 			'url'=>'<a href="/?oj=problems">Problems</a>',
@@ -24,6 +25,9 @@ function oj_maybe_redirect_url(){
 			'url' => '<a href="/?oj=submitpage">Submit Page</a>',
 			'url_raw' => '/?oj=submitpage',
 			'label' =>'Submit Page',
+		),
+		'addsolution' => array(
+			'url_raw' => '/?oj=addsolution'
 		)
 	);
 	$page=$_GET['oj'];
@@ -36,8 +40,7 @@ function oj_maybe_redirect_url(){
 			$top_page='status';
 			$oj_bread_trail['trail_end']='Status';
 			locate_template('oj-status-list.php',true);
-			exit(0);
-			break;
+			exit(0);break;
 		case 'statusd':
 			if($_GET['title']){
 				$top_page='problem';
@@ -50,22 +53,23 @@ function oj_maybe_redirect_url(){
 				$oj_bread_trail['trail_end']='Status Detail';
 			}
 			locate_template('oj-status-detail.php',true);
-			exit(0);
-			break;
+			exit(0);break;		
 		case 'problems':
 			$top_page='problem';
 			$oj_bread_trail['trail_end']='Problem';
 			locate_template('oj-problems.php',true);
-			exit(0);
-			break;
+			exit(0);break;
 		case 'submitpage':
 			$top_page='problem';
 			$oj_bread_trail[]=$page_urls['problem']['url'];
 			$oj_bread_trail[]='<a href="/?problem='.$_GET['title'].'">'.$_GET['title'].'</a>';
 			$oj_bread_trail['trail_end']='Submit Solution';
 			locate_template('oj-submitpage.php',true);
-			exit(0);
-			break;
+			exit(0);break;
+		case 'addsolution':
+			$top_page='home';
+			require_once (dirname(dirname(dirname(__FILE__))).'/addsolution.php');
+			exit(0);break;
 	}
 }
 ?>
