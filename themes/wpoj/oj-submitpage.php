0fc0818a7815b15006824a2234f530787624712c
johnnychq <johnnychq@gmail.com>
2011-06-05 13:16:18 +0000

    为什么我这么无聊捏？

--- themes/wpoj/oj-submitpage.php
+++ themes/wpoj/oj-submitpage.php
@@ -4,13 +4,13 @@ function echo_language_select($langmask,$default){
 	echo '<select id="language" name="language">';
 	$lang=(~((int)$langmask))&127;
 	
-	if(($lang&1)>0) echo"	    <option value=0 ".( $default==0?"selected":"").">C</option>";
-	if(($lang&2)>0) echo"		<option value=1 ".( $default==1?"selected":"").">C++</option>";
-	if(($lang&4)>0) echo"		<option value=2 ".( $default==2?"selected":"").">Pascal</option>";
-	if(($lang&8)>0) echo"		<option value=3 ".( $default==3?"selected":"").">Java</option>";
-	if(($lang&16)>0) echo"		<option value=4 ".( $default==4?"selected":"").">Ruby</option>";
-	if(($lang&32)>0) echo"		<option value=5 ".( $default==5?"selected":"").">Bash</option>";
-	if(($lang&64)>0) echo"		<option value=6 ".( $default==6?"selected":"").">Python</option>";
+	if(($lang&1)>0) 	echo"	<option value=0 ".( $default==0?"selected":"").">C</option>";
+	if(($lang&2)>0) 	echo"	<option value=1 ".( $default==1?"selected":"").">C++</option>";
+	if(($lang&4)>0) 	echo"	<option value=2 ".( $default==2?"selected":"").">Pascal</option>";
+	if(($lang&8)>0) 	echo"	<option value=3 ".( $default==3?"selected":"").">Java</option>";
+	if(($lang&16)>0) 	echo"	<option value=4 ".( $default==4?"selected":"").">Ruby</option>";
+	if(($lang&32)>0) 	echo"	<option value=5 ".( $default==5?"selected":"").">Bash</option>";
+	if(($lang&64)>0) 	echo"	<option value=6 ".( $default==6?"selected":"").">Python</option>";
 	echo '</select>';
 }
 ?>
@@ -49,7 +49,7 @@ function echo_language_select($langmask,$default){
 			Language : <?php echo_language_select($langmask, $lastlang)?>
 		</div>
 		<div>
-			<textarea cols=80 rows=20 id="source" name="source"><?=$source_code?></textarea>
+			<textarea cols=80 rows=20 id="source" style="height:308px;" name="source"><?=$source_code?></textarea>
 		</div>
 		<div>
 			<input type="submit" value="Submit"><input type="reset" value="Reset">
