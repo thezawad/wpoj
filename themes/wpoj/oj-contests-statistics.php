e06c9a3db3eed2caaacc364f835b027925bdb7b3
johnnychq <johnnychq@gmail.com>
2011-06-19 05:14:36 +0000

    adding contests-statistics page

--- themes/wpoj/oj-contests-statistics.php
+++ themes/wpoj/oj-contests-statistics.php
@@ -15,8 +15,81 @@ get_header('contest'); // Loads the header.php template. ?>
 	<div class="content-wrapper hentry clearfix">
 	<div id="content">
 
-	This is the statistics page
+	<?php 
+	global $oj,$oju,$wpdb;
+	$contest_id=$_GET['cid'];
+	
+	$sql="SELECT p2p_id cpid, p2p_to pid FROM {$wpdb->prefix}p2p WHERE p2p_from={$contest_id}";
+	$cps=$wpdb->get_results($sql);
+	
+	
+	$sql="SELECT result,cp_id,language	FROM {$oj->prefix}solution WHERE contest_id={$contest_id}";
+	$solutions=$wpdb->get_results($sql);
 
+	$problems=array();
+	foreach ($solutions as $sl){
+		$cpid=$sl->cp_id;
+		$result=$sl->result;
+		$lang=$sl->language;
+		//result statistics
+		if (isset($problems[$cpid]['result'][$result])){
+			$problems[$cpid]['result'][$result]++;
+		}else{
+			$problems[$cpid]['result'][$result]=1;
+		}
+		//language statistics
+		if (isset($problems[$cpid]['language'][$lang])){
+			$problems[$cpid]['language'][$lang]++;
+		}else{
+			$problems[$cpid]['language'][$lang]=1;
+		}
+	}
+	$compile_status=oj_get_compile_status();
+	$langs=array_keys($oj->languages);
+	
+	?>
+	<table class="tb-problems">
+		<caption>比赛题目列表-结果统计</caption>
+		<tr>
+			<td></td>
+			<?php for($i=4;$i<13;$i++){
+				echo '<th title="'.$compile_status['full'][$i].'">'.$compile_status['short'][$i].'</th>';
+			}?>
+		</tr>
+		<?php $p_name=65;
+		foreach ($cps as $cp ){ $cpid=$cp->cpid;?>
+		<tr>
+			<th><?php echo chr($p_name++);?></th>
+			<?php for($i=4;$i<13;$i++){
+				if (isset($problems[$cpid]['result'][$i])){
+					echo '<td>'.$problems[$cpid]['result'][$i].'</td>';
+				}else echo '<td></td>';
+			}?>
+		</tr>
+		<?php }?>
+	</table>
+	
+	<table class="tb-problems">
+		<caption>比赛题目列表-语言统计</caption>
+		<tr>
+			<td></td>
+			<?php for($i=0;$i<7;$i++){
+				echo '<th>'.$langs[$i].'</th>';
+			}?>
+		</tr>
+		<?php $p_name=65;
+		foreach ($cps as $cp ){ $cpid=$cp->cpid;?>
+		<tr>
+			<th><?php echo chr($p_name++);?></th>
+			<?php for($i=0;$i<7;$i++){
+				if (isset($problems[$cpid]['language'][$i])){
+					echo '<td>'.$problems[$cpid]['language'][$i].'</td>';
+				}else echo '<td></td>';
+			}?>
+		</tr>
+		<?php }?>
+	</table>
+	
 	</div><!-- #content -->
 	</div>
 	<?php do_atomic( 'after_content' ); // retro-fitted_after_content ?>
